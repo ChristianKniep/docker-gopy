4489e9d7b7edaf241604da31013d3888499f665e
Christian Kniep <christian@qnib.org>
2014-07-22 09:40:54 +0200

    First strike with a running goPy image

---
+++ bash_history
@@ -0,0 +1,5 @@
+gopy simple.go 
+python simple.py 
+gopy parallel.go 
+python parallel.py 
+
---
+++ simple.py
@@ -0,0 +1,8 @@
+import parallel
+import time
+
+x = []
+parallel.example(x)
+
+time.sleep(4)
+print "x =", x
